b0a31ac5d8f2210cd5d9b9d16dcd143380096849
Cornellius Bonongwe <127194596+bsc-com-ne-22-20@users.noreply.github.com>
2025-06-03 11:02:40 +0200

    Update LeafDiseasePredicter.py

---
+++ LeafDiseasePredicter.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+model = tf.keras.models.load_model("plant_disease_model.h5")
+
+labels = [
+    'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy',
+    'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_',
+    'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy',
+    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
+    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy',
+    'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy',
+    'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
+    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+    'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'
+]
+
+def preprocess_image(image_path):
+    image = Image.open(image_path).resize((224, 224))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+def predict_disease(image_path):
+    image = preprocess_image(image_path)
+    prediction = model.predict(image)
+    class_idx = np.argmax(prediction)
+    return labels[class_idx], prediction[0][class_idx]
+
+if __name__ == "__main__":
+    image_path = "sample_leaf.jpg"
+    result, confidence = predict_disease(image_path)
+    print(f"Disease: {result} ({confidence * 100:.2f}% confidence)")
